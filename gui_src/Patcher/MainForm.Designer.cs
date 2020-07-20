f4badd510729b392cc67aef331873aa5d54df3c3
Trevor Jackson <trevorj342@gmail.com>
2020-07-20 13:56:26 -0400

    Called Patcher

--- gui_src/Patcher/MainForm.Designer.cs
+++ gui_src/Patcher/MainForm.Designer.cs
@@ -34,6 +34,9 @@
             this.pathTextbox = new MetroFramework.Controls.MetroTextBox();
             this.unityVersions = new MetroFramework.Controls.MetroComboBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lightRadio = new MetroFramework.Controls.MetroRadioButton();
+            this.darkRadio = new MetroFramework.Controls.MetroRadioButton();
+            this.consoleOutput = new MetroFramework.Controls.MetroTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,32 +46,59 @@
             this.patchButton.Location = new System.Drawing.Point(23, 180);
             this.patchButton.Name = "patchButton";
             this.patchButton.Size = new System.Drawing.Size(437, 45);
-            this.patchButton.Style = MetroFramework.MetroColorStyle.Green;
+            this.patchButton.Style = MetroFramework.MetroColorStyle.White;
             this.patchButton.TabIndex = 24;
             this.patchButton.Text = "PATCH";
             this.patchButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.patchButton.UseSelectable = true;
+            this.patchButton.Click += new System.EventHandler(this.patchButton_Click);
             // 
             // selectUnity
             // 
             this.selectUnity.Location = new System.Drawing.Point(23, 145);
             this.selectUnity.Name = "selectUnity";
             this.selectUnity.Size = new System.Drawing.Size(75, 29);
-            this.selectUnity.Style = MetroFramework.MetroColorStyle.Green;
+            this.selectUnity.Style = MetroFramework.MetroColorStyle.White;
             this.selectUnity.TabIndex = 25;
             this.selectUnity.Text = "SELECT";
             this.selectUnity.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.selectUnity.UseSelectable = true;
             this.selectUnity.Click += new System.EventHandler(this.SelectClick);
             // 
             // pathTextbox
             // 
+            // 
+            // 
+            // 
+            this.pathTextbox.CustomButton.Image = null;
+            this.pathTextbox.CustomButton.Location = new System.Drawing.Point(197, 1);
+            this.pathTextbox.CustomButton.Name = "";
+            this.pathTextbox.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.pathTextbox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.pathTextbox.CustomButton.TabIndex = 1;
+            this.pathTextbox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.pathTextbox.CustomButton.UseSelectable = true;
+            this.pathTextbox.CustomButton.Visible = false;
+            this.pathTextbox.Lines = new string[] {
+        "Please select your Unity EXE..."};
             this.pathTextbox.Location = new System.Drawing.Point(104, 148);
+            this.pathTextbox.MaxLength = 32767;
             this.pathTextbox.Name = "pathTextbox";
+            this.pathTextbox.PasswordChar = '\0';
             this.pathTextbox.ReadOnly = true;
+            this.pathTextbox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.pathTextbox.SelectedText = "";
+            this.pathTextbox.SelectionLength = 0;
+            this.pathTextbox.SelectionStart = 0;
+            this.pathTextbox.ShortcutsEnabled = true;
             this.pathTextbox.Size = new System.Drawing.Size(219, 23);
-            this.pathTextbox.Style = MetroFramework.MetroColorStyle.Green;
+            this.pathTextbox.Style = MetroFramework.MetroColorStyle.White;
             this.pathTextbox.TabIndex = 23;
             this.pathTextbox.Text = "Please select your Unity EXE...";
             this.pathTextbox.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.pathTextbox.UseSelectable = true;
+            this.pathTextbox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.pathTextbox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
             // 
             // unityVersions
             // 
@@ -86,13 +116,14 @@
             this.unityVersions.Location = new System.Drawing.Point(329, 145);
             this.unityVersions.Name = "unityVersions";
             this.unityVersions.Size = new System.Drawing.Size(131, 29);
-            this.unityVersions.Style = MetroFramework.MetroColorStyle.Green;
+            this.unityVersions.Style = MetroFramework.MetroColorStyle.White;
             this.unityVersions.TabIndex = 22;
             this.unityVersions.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.unityVersions.UseSelectable = true;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::Patcher.Properties.Resources.external_content_duckduckgo;
+            this.pictureBox1.Image = global::Patcher.Properties.Resources.UnityLogo;
             this.pictureBox1.Location = new System.Drawing.Point(180, 18);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(108, 113);
@@ -100,11 +131,73 @@
             this.pictureBox1.TabIndex = 26;
             this.pictureBox1.TabStop = false;
             // 
+            // lightRadio
+            // 
+            this.lightRadio.Appearance = System.Windows.Forms.Appearance.Button;
+            this.lightRadio.AutoSize = true;
+            this.lightRadio.Location = new System.Drawing.Point(371, 124);
+            this.lightRadio.Name = "lightRadio";
+            this.lightRadio.Size = new System.Drawing.Size(89, 15);
+            this.lightRadio.TabIndex = 27;
+            this.lightRadio.Text = "Light Theme";
+            this.lightRadio.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.lightRadio.UseSelectable = true;
+            // 
+            // darkRadio
+            // 
+            this.darkRadio.AutoSize = true;
+            this.darkRadio.Checked = true;
+            this.darkRadio.Location = new System.Drawing.Point(374, 102);
+            this.darkRadio.Name = "darkRadio";
+            this.darkRadio.Size = new System.Drawing.Size(86, 15);
+            this.darkRadio.TabIndex = 28;
+            this.darkRadio.TabStop = true;
+            this.darkRadio.Text = "Dark Theme";
+            this.darkRadio.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.darkRadio.UseSelectable = true;
+            // 
+            // consoleOutput
+            // 
+            // 
+            // 
+            // 
+            this.consoleOutput.CustomButton.Image = null;
+            this.consoleOutput.CustomButton.Location = new System.Drawing.Point(147, 1);
+            this.consoleOutput.CustomButton.Name = "";
+            this.consoleOutput.CustomButton.Size = new System.Drawing.Size(289, 289);
+            this.consoleOutput.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.consoleOutput.CustomButton.TabIndex = 1;
+            this.consoleOutput.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.consoleOutput.CustomButton.UseSelectable = true;
+            this.consoleOutput.CustomButton.Visible = false;
+            this.consoleOutput.Lines = new string[0];
+            this.consoleOutput.Location = new System.Drawing.Point(23, 240);
+            this.consoleOutput.MaxLength = 9999999;
+            this.consoleOutput.Multiline = true;
+            this.consoleOutput.Name = "consoleOutput";
+            this.consoleOutput.PasswordChar = '\0';
+            this.consoleOutput.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.consoleOutput.SelectedText = "";
+            this.consoleOutput.SelectionLength = 0;
+            this.consoleOutput.SelectionStart = 0;
+            this.consoleOutput.ShortcutsEnabled = true;
+            this.consoleOutput.Size = new System.Drawing.Size(437, 291);
+            this.consoleOutput.Style = MetroFramework.MetroColorStyle.White;
+            this.consoleOutput.TabIndex = 29;
+            this.consoleOutput.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.consoleOutput.UseSelectable = true;
+            this.consoleOutput.Visible = false;
+            this.consoleOutput.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.consoleOutput.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
             // PatcherForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(492, 256);
+            this.Controls.Add(this.consoleOutput);
+            this.Controls.Add(this.darkRadio);
+            this.Controls.Add(this.lightRadio);
             this.Controls.Add(this.patchButton);
             this.Controls.Add(this.selectUnity);
             this.Controls.Add(this.pathTextbox);
@@ -121,6 +214,7 @@
             this.Load += new System.EventHandler(this.PatcherFormLoad);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -131,6 +225,9 @@
         public MetroFramework.Controls.MetroTextBox pathTextbox;
         public MetroFramework.Controls.MetroComboBox unityVersions;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroRadioButton lightRadio;
+        private MetroFramework.Controls.MetroRadioButton darkRadio;
+        private MetroFramework.Controls.MetroTextBox consoleOutput;
     }
 }
 
--- gui_src/Patcher/MainForm.cs
+++ gui_src/Patcher/MainForm.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using MetroFramework;
+using System;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Patcher
 {
     public partial class PatcherForm : MetroFramework.Forms.MetroForm
     {
-        string unityExePath;
+        private string unityExePath;
 
         public PatcherForm()
         {
@@ -30,15 +24,77 @@ namespace Patcher
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
-                openFileDialog.Filter = "Unity Editor (*.exe)|*.exe";
+                openFileDialog.Filter = "Unity Editor|Unity.exe";
                 openFileDialog.RestoreDirectory = true;
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     unityExePath = openFileDialog.FileName;
                     pathTextbox.Text = unityExePath;
+
+                    patchButton.Enabled = true;
                 }
             }
+        }
+
+        private void patchButton_Click(object sender, EventArgs e)
+        {
+            string theme = null;
+
+            if (darkRadio.Checked)
+                theme = "dark";
+            else if (lightRadio.Checked)
+                theme = "light";
+            else
+                Error("Theme has not been selected, please select one and try again.");
+
+            ShowConsole();
+
+            #region StartInfo
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.ErrorDialog = false;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.FileName = "Patcher.exe";
+            processStartInfo.Arguments = $"--windows --version={unityVersions.SelectedItem} --t={theme} --e={unityExePath}";
+            #endregion
+
+            #region Execution
+            Process process = new Process();
+            process.StartInfo = processStartInfo;
+            bool processStarted = process.Start();
+            if (processStarted)
+            {
+                StreamReader outputReader = process.StandardOutput;
+                process.WaitForExit();
+
+                string displayText = "";
+                displayText += outputReader.ReadToEnd();
+                consoleOutput.Text = displayText;
+            }
+            #endregion
+        }
+
+        private void ShowConsole()
+        {
+            // Original Size: 492, 256
+            Height = 544;
+            consoleOutput.Visible = true;
+        }
+
+        private void Error(string message)
+        {
+            var msgBox = MetroMessageBox.Show(this, message, "An error has occured", MessageBoxButtons.OKCancel, 120);
+            if (msgBox == DialogResult.Cancel)
+                Environment.Exit(0);
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
